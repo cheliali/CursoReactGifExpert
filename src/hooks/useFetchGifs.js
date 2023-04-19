f505bb2c2424ae7870e4276a23f51a25143d4396
Chelsea <cheliali@yahoo.com>
2023-04-18 19:12:53 -0500

    First Commit

---
+++ src/hooks/useFetchGifs.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+import { getGifs } from "../helpers/getGifs";
+
+export const useFetchGifs = (category) => {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const getImages = async () => {
+    const newImages = await getGifs(category);
+    setImages(newImages);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    getImages();
+  }, []);
+
+  // getGifs(category); //NO EJECUCION DE FUNCIONES DENTRO DE UN FUNCIONAL COMPONENT
+
+  return {
+    images,
+    isLoading,
+  };
+};
